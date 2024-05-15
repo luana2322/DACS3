65c25d33b83dc3dd83dda669b2a16ef305217cb6
luana2322 <nguyenbaoluanthai@gmail.com>
2024-05-15 20:46:43 +0700

    validation userdto

--- workschedule/src/main/java/com/workschedule/controller/UserController.java
+++ workschedule/src/main/java/com/workschedule/controller/UserController.java
@@ -3,8 +3,10 @@ package com.workschedule.controller;
 import com.workschedule.dto.UserDto;
 import com.workschedule.model.Users;
 import com.workschedule.service.serviceImpl.UserServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,11 @@ public class UserController {
     private UserServiceImpl userServiceImpl;
 
 @PostMapping("/register")
-    public ResponseEntity<Users> register(@RequestBody UserDto userDto){
-
+    public ResponseEntity<Users> register(@RequestBody @Valid UserDto userDto
+        ,BindingResult bindingResult ) throws Exception {
+if (bindingResult.hasErrors()){
+    throw new  Exception("Wrong data");
+}
     return ResponseEntity.ok(userServiceImpl.save(userDto));
 }
 
--- workschedule/src/main/java/com/workschedule/dto/UserDto.java
+++ workschedule/src/main/java/com/workschedule/dto/UserDto.java
@@ -1,5 +1,8 @@
 package com.workschedule.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 @Getter
@@ -8,8 +11,13 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class UserDto {
+    @Email(message = "Email không hợp lệ")
 String emaildto;
+    @NotEmpty(message = "Thiếu password")
+    @Min(value = 8, message = "Password phải từ 8 kí tự trở lên")
 String passworddto;
-String firstName;
+    @Min(value = 1, message = "FirstName không được để trống")
+    String firstName;
+    @Min(value = 1, message = "LastName không được để trống")
 String lastName;
 }
