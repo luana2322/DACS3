2109f039c1711c65bd749faa4459ec97e79c4aaa
luana2322 <nguyenbaoluanthai@gmail.com>
2024-06-15 05:27:03 +0700

    add comment function

--- workschedule/src/main/java/com/workschedule/controller/CommentController.java
+++ workschedule/src/main/java/com/workschedule/controller/CommentController.java
@@ -2,7 +2,12 @@ package com.workschedule.controller;
 
 import com.workschedule.model.Comment;
 import com.workschedule.model.Project;
+import com.workschedule.model.Task;
+import com.workschedule.model.Users;
+import com.workschedule.repository.CommentRepository;
 import com.workschedule.service.serviceImpl.CommentServiceImpl;
+import com.workschedule.service.serviceImpl.TaskServiceImpl;
+import com.workschedule.service.serviceImpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +18,22 @@ import java.util.List;
 public class CommentController {
     @Autowired
     private CommentServiceImpl commentServiceImpl;
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private TaskServiceImpl taskServiceImpl;
+    @Autowired
+    private UserServiceImpl userServiceImpl;
 
 
-    @PostMapping("/addcomment")
-    public ResponseEntity<Comment> addcomment(@RequestBody Comment comment) {
+    @PostMapping("/postcomment")
+    public ResponseEntity<Comment> postcomment(@RequestBody Comment comment,
+                                               @RequestParam("taskId") Long taskId,
+                                               @RequestParam("userId") Long userId) {
+        Task task=taskServiceImpl.findById(taskId);
+        comment.setTask(task);
+        Users user=userServiceImpl.findById(userId);
+        comment.setUsers(user);
         return ResponseEntity.ok(commentServiceImpl.save(comment));
     }
 
@@ -36,6 +53,11 @@ public class CommentController {
     public ResponseEntity<Comment> findbyid(@RequestParam("commentId") Long commentId) {
 
         return ResponseEntity.ok(commentServiceImpl.findById(commentId));
+    }
+
+    @GetMapping("/getallcommentbyTask")
+    public ResponseEntity<List<Comment>> getallcommentbyTask(@RequestParam("taskId") Long taskId) {
+        return ResponseEntity.ok(commentRepository.getallcommentbytaskidoject(taskId));
     }
 
 
